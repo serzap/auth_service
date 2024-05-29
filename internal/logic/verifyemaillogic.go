de4ad207eaaebf8ced538e55c336ac616ade81e2
Serhii Zaporozhchenko <serhii.zaporozhchenko@gmail.com>
2024-05-29 17:12:43 +0100

    fix: verify code by email

--- internal/logic/verifyemaillogic.go
+++ internal/logic/verifyemaillogic.go
@@ -2,6 +2,7 @@ package logic
 
 import (
 	"context"
+	"database/sql"
 	"errors"
 
 	"github.com/serzap/auth_service/api"
@@ -9,6 +10,11 @@ import (
 	"github.com/serzap/auth_service/model"
 
 	"github.com/zeromicro/go-zero/core/logx"
+)
+
+var (
+	ErrUserNotFound            = errors.New("user not found")
+	ErrInvalidVerificationCode = errors.New("invalid verification code")
 )
 
 type VerifyEmailLogic struct {
@@ -26,28 +32,27 @@ func NewVerifyEmailLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Verif
 }
 
 func (l *VerifyEmailLogic) VerifyEmail(in *api.VerifyEmailRequest) (*api.VerifyEmailResponse, error) {
-	user, err := l.svcCtx.UsersModel.FindByVerificationCode(l.ctx, in.VerificationCode)
+	user, err := l.svcCtx.UsersModel.FindOneByEmail(l.ctx, in.Email)
 	if err != nil {
 		if err == model.ErrNotFound {
-			return nil, errors.New("verification code not found")
+			logx.Errorf("User with email %s not found", in.Email)
+			return nil, ErrUserNotFound
 		}
 		return nil, err
 	}
 
-	valid, err := l.svcCtx.UsersModel.IsVerificationCodeValid(l.ctx, in.VerificationCode)
-
-	if err != nil {
-		return nil, err
-	}
-	if !valid {
-		return nil, err
+	if user.VerificationCode.String != in.VerificationCode || !user.VerificationCode.Valid {
+		logx.Errorf("Invalid verification code for user %s", in.Email)
+		return nil, ErrInvalidVerificationCode
 	}
 
 	user.Verified = true
+	user.VerificationCode = sql.NullString{String: "", Valid: false} // Очистить verification code
 	err = l.svcCtx.UsersModel.Update(l.ctx, user)
 	if err != nil {
 		return nil, err
 	}
 
+	logx.Infof("User %s successfully verified", in.Email)
 	return &api.VerifyEmailResponse{}, nil
 }
--- model/usersmodel.go
+++ model/usersmodel.go
@@ -1,11 +1,6 @@
 package model
 
 import (
-	"context"
-	"database/sql"
-	"errors"
-	"fmt"
-
 	"github.com/zeromicro/go-zero/core/stores/sqlx"
 )
 
@@ -17,8 +12,6 @@ type (
 	UsersModel interface {
 		usersModel
 		withSession(session sqlx.Session) UsersModel
-		FindByVerificationCode(ctx context.Context, code string) (*Users, error)
-		IsVerificationCodeValid(ctx context.Context, code string) (bool, error)
 	}
 
 	customUsersModel struct {
@@ -35,28 +28,4 @@ func NewUsersModel(conn sqlx.SqlConn) UsersModel {
 
 func (m *customUsersModel) withSession(session sqlx.Session) UsersModel {
 	return NewUsersModel(sqlx.NewSqlConnFromSession(session))
-}
-
-func (m *customUsersModel) FindByVerificationCode(ctx context.Context, code string) (*Users, error) {
-	var user Users
-	query := fmt.Sprintf("select %s from %s where `verification_code` = ? limit 1", usersRows, m.table)
-	err := m.conn.QueryRowCtx(ctx, query, code)
-	switch {
-	case err == nil:
-		return &user, nil
-	case errors.Is(err, sql.ErrNoRows):
-		return nil, ErrNotFound
-	default:
-		return nil, err
-	}
-}
-
-func (m *customUsersModel) IsVerificationCodeValid(ctx context.Context, code string) (bool, error) {
-	query := fmt.Sprintf("select count(*) from %s where `verification_code` = ? and `verified` = ?", m.table)
-	var count int
-	err := m.conn.QueryRowCtx(ctx, query, code, true)
-	if err != nil {
-		return false, err
-	}
-	return count > 0, nil
 }
